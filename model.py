2d40e08886d59737cd8739a203e6ebf46ed5e4a0
sallto <look@mygithub.email>
2023-12-29 12:14:57 +0100

    eanrtendait

---
+++ model.py
@@ -0,0 +1,8 @@
+import numpy as np  
+import pandas as pd  
+
+diet = pd.read_csv("diet.csv")
+recipes= pd.read_csv("recipes.csv")
+requests=pd.read_csv("requests.csv")
+reviews=pd.read_csv("reviews.csv")
+
